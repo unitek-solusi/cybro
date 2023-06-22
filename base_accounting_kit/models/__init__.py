d974b54afb56c1eba29b159e70e19492fd250ea9
Sysadmin <sysadmin@uniteksolusi.com>
2023-06-22 17:48:37 +0700

    UPD remove reconciliation
    Remove Reconciliation to make it compatible with OCA Reconciliation

--- base_accounting_kit/models/__init__.py
+++ base_accounting_kit/models/__init__.py
@@ -23,7 +23,7 @@
 from . import account_account
 from . import account_asset
 from . import account_followup
-from . import account_journal
+#from . import account_journal
 from . import account_move
 from . import account_payment
 from . import credit_limit
@@ -32,6 +32,6 @@ from . import recurring_payments
 from . import res_config_settings
 from . import res_partner
 from . import account_dashboard
-from . import payment_matching
+#from . import payment_matching
 from . import multiple_invoice
 from . import multiple_invoice_layout
